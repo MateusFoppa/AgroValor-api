6a98f6bd45dd89a32fa70e63e9ce8f1cfd27eabb
MateusFoppa <mateusfoppa@outlook.com>
2023-09-11 17:07:38 -0300

    :goal_net: feat: Add custom error

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,8 +2,11 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello Dev!' });
-});
+routes.get(
+  '/',
+  (_request: any, response: { json: (arg0: { message: string }) => any }) => {
+    return response.json({ message: 'Hello Dev!' });
+  },
+);
 
 export default routes;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
+import errorHandlerMiddleware from './middlewares/error-handler';
 
 const app = express();
 
@@ -8,6 +9,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errorHandlerMiddleware);
 
 app.listen(3333, () => {
   console.log('Server starter o port 3333! 👍');
