c11e1a15bc36a03e08430fed80c9efe39751b2c2
MateusFoppa <55770663+MateusFoppa@users.noreply.github.com>
2023-09-14 12:18:16 -0300

    :sparkles:	feat: Entities Property

---
+++ src/modules/propertys/domain/models/IProperty.ts
@@ -0,0 +1,11 @@
+export interface IProperty {
+  id: string;
+  name: string;
+  user_id: string;
+  total_area: number;
+  cultivated_area: number;
+  city: string;
+  state: string;
+  created_at: Date;
+  updated_at: Date;
+}
---
+++ src/modules/propertys/typeorm/entities/Property.ts
@@ -0,0 +1,41 @@
+import { IProperty } from '@modules/propertys/domain/models/IProperty';
+// import User from '@modules/user/infra/typeorm/entities/User';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('propertys')
+class Property implements IProperty {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ nullable: true })
+  user_id: string;
+
+  @Column()
+  total_area: number;
+
+  @Column()
+  cultivated_area: number;
+
+  @Column()
+  city: string;
+
+  @Column()
+  state: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Property;
