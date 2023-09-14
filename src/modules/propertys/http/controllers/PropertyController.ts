7dbbb6bda36b15fee490e7e2146a26777bd6d5b6
MateusFoppa <55770663+MateusFoppa@users.noreply.github.com>
2023-09-14 15:05:14 -0300

    :✨: feat: CreatePropertyController

---
+++ src/modules/propertys/http/controllers/PropertyController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import { StatusCodes } from 'http-status-codes';
+import CreatePropertyService from '@modules/propertys/services/CreatePropertyService';
+
+export default class ProductsController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, total_area, cultivated_area, city, state } = request.body;
+    const { user_id } = request.params;
+
+    const createProperty = container.resolve(CreatePropertyService);
+
+    const property = await createProperty.execute({
+      name,
+      user_id,
+      total_area,
+      cultivated_area,
+      city,
+      state,
+    });
+
+    return response.status(StatusCodes.CREATED).json(property);
+  }
+}
