ee810826ddacab30ca70bca2dcf8fd68359ed357
MateusFoppa <55770663+MateusFoppa@users.noreply.github.com>
2023-09-15 12:43:20 -0300

    :✨ feat: Route List Property

--- src/modules/propertys/infra/http/routes/propertys.routes.ts
+++ src/modules/propertys/infra/http/routes/propertys.routes.ts
@@ -4,6 +4,7 @@ import PropertyController from '../controllers/PropertyController';
 const propertyRouter = Router();
 const propertyController = new PropertyController();
 
+propertyRouter.get('/user', propertyController.listAll);
 propertyRouter.post('/', propertyController.create);
 
 export default propertyRouter;
