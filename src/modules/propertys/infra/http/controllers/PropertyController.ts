b44af7405262b4e17112513dc134da0c0634af9a
MateusFoppa <55770663+MateusFoppa@users.noreply.github.com>
2023-09-15 12:36:56 -0300

    :✨: feat: ListPropertyController

--- src/modules/propertys/infra/http/controllers/PropertyController.ts
+++ src/modules/propertys/infra/http/controllers/PropertyController.ts
@@ -2,8 +2,21 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import { StatusCodes } from 'http-status-codes';
 import CreatePropertyService from '@modules/propertys/services/CreatePropertyService';
+import ListPropertyService from '@modules/propertys/services/ListPropertyService';
 
 export default class ProductsController {
+  public async listAll(
+    request: Request,
+    response: Response,
+  ): Promise<Response> {
+    const user_id = request.user.id;
+
+    const listPropertys = container.resolve(ListPropertyService);
+    const propertys = await listPropertys.execute(user_id);
+
+    return response.status(StatusCodes.OK).json(propertys);
+  }
+
   public async create(request: Request, response: Response): Promise<Response> {
     const { name, total_area, cultivated_area, city, state } = request.body;
 
