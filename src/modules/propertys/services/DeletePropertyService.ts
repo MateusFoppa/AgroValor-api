c5eeb3d101b855ab8ff8844cf4d2d4f7d70f556e
MateusFoppa <55770663+MateusFoppa@users.noreply.github.com>
2023-09-15 16:07:32 -0300

    :✨: feat: Service Delete Property

---
+++ src/modules/propertys/services/DeletePropertyService.ts
@@ -0,0 +1,28 @@
+import { inject, injectable } from 'tsyringe';
+import { IPropertyRepository } from '../domain/repositories/IPropertyRepository';
+import { IDeleteProperty } from '../domain/models/IDeleteProperty';
+import CustomAPIError from '@shared/errors';
+
+@injectable()
+class DeletePropertyService {
+  constructor(
+    @inject('PropertyRepository')
+    private propertyRepository: IPropertyRepository,
+  ) {
+    if (!propertyRepository) {
+      throw new Error('PropertyRepository is required.');
+    }
+  }
+
+  public async execute({ id }: IDeleteProperty): Promise<void> {
+    const product = await this.propertyRepository.findById(id);
+
+    if (!product) {
+      throw new CustomAPIError.BadRequestError('Product not found.');
+    }
+
+    await this.propertyRepository.remove(product);
+  }
+}
+
+export default DeletePropertyService;
