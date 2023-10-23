a44723498f38abd998a95ff640a3902fb3320f98
MateusFoppa <55770663+MateusFoppa@users.noreply.github.com>
2023-10-23 14:48:58 -0300

    :✨: feat: Implements Repository

---
+++ src/modules/expenses/infra/typeorm/repositories/ExpensesRepository.ts
@@ -0,0 +1,65 @@
+import { Repository } from 'typeorm';
+import Expenses from '../entities/Expense';
+import { IExpensesRepository } from '@modules/expenses/domain/repositories/IExpensesRepository';
+import { dataSource } from '@shared/infra/typeorm';
+import { IListExpenses } from '@modules/expenses/domain/models/iListExpenses';
+import { IExpenses } from '@modules/expenses/domain/models/IExpenses';
+import { ICreateExpenses } from '@modules/expenses/domain/models/ICreateExpenses';
+
+class ExpensesRepository implements IExpensesRepository {
+  private ormRepository: Repository<Expenses>;
+
+  constructor() {
+    this.ormRepository = dataSource.getRepository(Expenses);
+  }
+
+  public async findAll(batch_id: string): Promise<IListExpenses> {
+    const expenses = await this.ormRepository.find({ where: { batch_id } });
+    const result: IListExpenses = { data: expenses };
+    return result;
+  }
+
+  public async findByName(
+    name: string,
+    batch_id: string,
+  ): Promise<IExpenses | null> {
+    const expense = await this.ormRepository.findOne({
+      where: { name, batch_id },
+    });
+    return expense || null;
+  }
+
+  public async findByIds(
+    expense_id: string,
+    batch_id: string,
+  ): Promise<IExpenses | null> {
+    const expense = await this.ormRepository.findOne({
+      where: { id: expense_id, batch_id },
+    });
+    return expense;
+  }
+
+  public async findById(id: string): Promise<IExpenses | null> {
+    const expense = await this.ormRepository.findOneBy({
+      id,
+    });
+    return expense;
+  }
+
+  public async create(data: ICreateExpenses): Promise<IExpenses> {
+    const expense = this.ormRepository.create(data);
+    await this.ormRepository.save(expense);
+    return expense;
+  }
+
+  public async save(expense: IExpenses): Promise<IExpenses> {
+    await this.ormRepository.save(expense);
+    return expense;
+  }
+
+  public async remove(expense: Expenses): Promise<void> {
+    await this.ormRepository.remove(expense);
+  }
+}
+
+export default ExpensesRepository;
