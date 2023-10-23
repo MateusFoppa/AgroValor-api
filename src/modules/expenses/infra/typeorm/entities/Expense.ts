32cb8e6f1d340c1675a9614c2aca463b06a52237
MateusFoppa <55770663+MateusFoppa@users.noreply.github.com>
2023-10-23 14:04:52 -0300

    :✨: feat: Entities expenses

---
+++ src/modules/expenses/infra/typeorm/entities/Expense.ts
@@ -0,0 +1,57 @@
+import Batch from '@modules/batch/infra/typeorm/entities/Batch';
+import { IExpenses } from '@modules/expenses/domain/models/IExpenses';
+
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+@Entity('expense')
+class Expenses implements IExpenses {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ nullable: true })
+  batch_id: string;
+
+  @Column()
+  category: string;
+
+  @Column()
+  item: string;
+
+  @Column()
+  unit_of: string;
+
+  @Column()
+  quantity: number;
+
+  @Column()
+  value_unit: number;
+
+  @Column()
+  value_total: number;
+
+  @Column()
+  data_pgto: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @ManyToOne(() => Batch)
+  @JoinColumn({ name: 'property_id' })
+  batch: Batch;
+}
+
+export default Expenses;
