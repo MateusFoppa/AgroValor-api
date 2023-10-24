f82a86c17293d41513ba50a26129ae864ac33eca
MateusFoppa <55770663+MateusFoppa@users.noreply.github.com>
2023-10-24 13:28:44 -0300

    :✨: feat: Update Expenses

--- src/modules/expenses/infra/http/controller/ExpenseController.ts
+++ src/modules/expenses/infra/http/controller/ExpenseController.ts
@@ -1,5 +1,6 @@
 import CreateExpenseService from '@modules/expenses/services/CreateExpenseService';
 import ListExpenseService from '@modules/expenses/services/ListExpenseService';
+import UpdateExpenseService from '@modules/expenses/services/UpdateExpenseService';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { container } from 'tsyringe';
@@ -40,5 +41,35 @@ export default class ExpenseController {
     });
 
     return response.status(StatusCodes.CREATED).json(expense);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const {
+      category,
+      item,
+      unit_of,
+      quantity,
+      value_unit,
+      value_total,
+      data_pgto,
+    } = request.body;
+    const batch_id = request.params.batch_id;
+    const expenses_id = request.params.expense_id;
+
+    const updateExpense = container.resolve(UpdateExpenseService);
+
+    const expense = await updateExpense.execute({
+      expenses_id,
+      batch_id,
+      category,
+      item,
+      unit_of,
+      quantity,
+      value_unit,
+      value_total,
+      data_pgto,
+    });
+
+    return response.status(StatusCodes.OK).json(expense);
   }
 }
--- src/modules/expenses/infra/http/routes/expense.routes.ts
+++ src/modules/expenses/infra/http/routes/expense.routes.ts
@@ -9,5 +9,6 @@ expenseRouter.use(isAuthenticated);
 
 expenseRouter.get('/batch/:batch_id', expenseController.index);
 expenseRouter.post('/batch/:batch_id', expenseController.create);
+expenseRouter.put('/:expense_id/batch/:batch_id', expenseController.update);
 
 export default expenseRouter;
